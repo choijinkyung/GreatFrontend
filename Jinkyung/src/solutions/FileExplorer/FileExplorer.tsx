a161c1f358bb1fab9efdc33b16317fab0eeaa77d
jkchoi <jkchoi@suredatalab.com>
2025-09-05 16:19:22 +0900

    [week3] file Explorer add aria

--- Jinkyung/src/solutions/FileExplorer/FileExplorer.tsx
+++ Jinkyung/src/solutions/FileExplorer/FileExplorer.tsx
@@ -1,27 +1,27 @@
 import { useState } from "react";
 
-export default function FileExplorer({ data }) {
+export default function FileExplorer({ data,level=1 }) {
   return (
-    <div>
-      {data.map((node) => (
-        <FileNode node={node} />
+    <ul role={level===1?'tree':'group'} aria-label={level === 1 ? "File Explorer" : undefined}>
+      {data.map((node,index) => (
+        <FileNode node={node} key={node.id} level={level} posInSet={index+1} setSize={data.length}/>
       ))}
-    </div>
+    </ul>
   );
 }
 
-function FileNode({ node }) {
+function FileNode({ node ,level,posInSet,setSize }) {
   const isFolder = node.children;
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div style={{ marginLeft: "10px" }}>
+    <li style={{ marginLeft: "10px" }} role="treeitem" aria-level={level} aria-posInSet={posInSet} aria-setSize={setSize} aria-expanded={isFolder?isOpen:undefined}>
       <div
         style={{ fontWeight: isFolder ? "bold" : "" }}
         onClick={() => isFolder && setIsOpen(!isOpen)}
       >
-        {node.name} {isFolder ? "[+]" : ""}
+        {node.name} {isFolder ? (isOpen?'[-]':'[+]') : ""}
       </div>
-      <div>{isFolder && isOpen && <FileExplorer data={node.children} />}</div>
-    </div>
+      <div>{isFolder && isOpen && <div role="group"><FileExplorer data={node.children} level={level+1}/></div>}</div>
+    </li>
   );
 }
