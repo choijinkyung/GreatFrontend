f3b3f030dd2789cbcff7efb248fc2124f3c3adef
jkchoi <jkchoi@suredatalab.com>
2025-09-25 17:22:19 +0900

    [week5] AutoComplete

--- Jinkyung/src/solutions/Autocomplete/Demo.tsx
+++ Jinkyung/src/solutions/Autocomplete/Demo.tsx
@@ -1,42 +1,120 @@
 import * as React from 'react';
-import top100Films from './top100Films'
+import top100Films from './top100Films';
+
 export default function ComboBox() {
-  const [isOpen, setIsOpen ] =React.useState(false)
-  const [searchFilter, setSearchFilter] = React.useState('')
-  const [filterFilms,setFilterFilms] = React.useState([])
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [searchFilter, setSearchFilter] = React.useState('');
+  const [filterFilms, setFilterFilms] = React.useState([]);
 
-  const handleClick =()=>{
-    setIsOpen(!isOpen)
-  }
-  const handleChange=(e)=>{
-    e.preventDefault()
-    console.log(e.target.value)
-
-    setSearchFilter(e.target.value)
+  function handleChange(e) {
+    e.preventDefault();
+    setSearchFilter(e.target.value);
+    setIsOpen(true);
   }
 
-  const handleLabelClick=(value)=>{
-    console.log(value)
-    setSearchFilter(value.label)
+  function handleLabelClick(value) {
+    setSearchFilter(value.label);
+    setIsOpen(false);
   }
-  
-  React.useEffect(()=>{
-    setFilterFilms(top100Films.filter((v) => v.label.includes(searchFilter)))
-  }, [searchFilter])
+
+  React.useEffect(() => {
+    setFilterFilms(
+      top100Films.filter((v) =>
+        v.label.toLowerCase().includes(searchFilter.toLowerCase())
+      )
+    );
+  }, [searchFilter]);
 
   return (
-    <div >
-      <input  type="text" style={{ width: '300px', height: '60px', border: '1px solid black' }} onClick={handleClick}
-        onChange={(e) =>{handleChange(e)}} value={searchFilter}>
-      </input>
-      <div>  {isOpen && filterFilms.map((value) => {
-        return (<div >{<div onClick={() => handleLabelClick(value)}>{value.label} - {value.year}</div>}</div>)
-      })}</div>
+    <div>
+      <input
+        onClick={() => setIsOpen((prev) => !prev)}
+        style={{
+          width: '300px',
+          height: '30px',
+          border: '1px solid black',
+          position: 'relative',
+        }}
+        value={searchFilter}
+        onChange={(e) => handleChange(e)}
+      ></input>
+      {searchFilter && (
+        <button
+          style={{
+            position: 'absolute',
+            left: '250px',
+            top: '10px',
+            border: 'none',
+            borderRadius: '100%',
+            width: '30px',
+            height: '30px',
+          }}
+          onClick={() => {
+            setSearchFilter('');
+            setIsOpen(false);
+          }}
+        >
+          X
+        </button>
+      )}
+      <button
+        style={{
+          position: 'absolute',
+          left: '280px',
+          top: '10px',
+          border: 'none',
+          borderRadius: '100%',
+          width: '30px',
+          height: '30px',
+        }}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {isOpen ? '▼' : '▲'}
+      </button>
+
+      {isOpen && (
+        <ul
+          style={{
+            backgroundColor: 'gray',
+            borderRadius: '10px',
+            marginTop: '2px',
+            position: 'absolute',
+            width: '300px',
+            overflow: 'auto',
+          }}
+        >
+          {filterFilms.map((value) => (
+            <li
+              onClick={() => handleLabelClick(value)}
+              style={{
+                padding: '10px',
+                listStyle: 'none',
+                cursor: 'pointer',
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.backgroundColor = 'red')
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.backgroundColor = 'gray')
+              }
+            >
+              {value.label}-{value.year}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-//filter top100Films - searchFilter 
-
-//isOpen 위치, filter, input, state, string함수 includes 
+/**
+ * 1. make input field
+ * 2. show top100films list
+ * 3. state for list open
+ * 4. click event for open
+ * 5. input field onChange for filter
+ *   -> filter search
+ *   -> lowercase, (debounce, throttle)
+ * 6. list click event -> change value
+ */
 
