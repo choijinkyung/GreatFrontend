442ec840fa2fff3336648df94fdbfbbafcd81643
jkchoi <jkchoi@suredatalab.com>
2025-09-26 11:14:42 +0900

    [week5]Like Button

--- Jinkyung/src/solutions/LikeButton/App.js
+++ Jinkyung/src/solutions/LikeButton/App.js
@@ -1,49 +1,83 @@
 import { HeartIcon, SpinnerIcon } from "./icons";
 import { useState } from "react";
-export default function App() {
-  const [loading, setLoading] = useState(false);
-  const [liked, setLiked] = useState(false);
 
-  const handleClick = async () => {
-    setLoading(true);
-    const body = {
-      action: "like",
-    };
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState("default");
+
+  //state for button state
+  //call api function
+  //
+  async function handleAction(action) {
+    setStatus("loading");
+
     try {
-     await fetch(
-        `https://questions.greatfrontend.com/api/questions/like-button`,
+      const response = await fetch(
+        "https://questions.greatfrontend.com/api/questions/like-button",
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body),
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ action }),
         },
-      ).then((res) => {
-        setLiked(true);
-      });
+      );
+      const result = await response.json();
+
+      if (response.ok) {
+        setMessage("");
+        setStatus(action === "like" ? "liked" : "default");
+      } else {
+        setMessage(result.message);
+        setStatus("default");
+      }
     } catch (error) {
-      setLiked(false);
-    } finally {
-      setLoading(false);
+      console.error(error);
+      setMessage("Network error");
+      setStatus("default");
     }
-  };
+  }
   return (
-    <div>
+    <div className="wrapper">
       <button
-        className={!liked ? "default-button" : "liked-button"}
-        onClick={() => handleClick()}
+        onClick={() => handleAction(status === "liked" ? "unlike" : "like")}
+        className={status === "default" ? "default-button" : "like-button"}
+        disabled={status === "loading"}
       >
-        {!loading ? <HeartIcon /> : <SpinnerIcon />}Like
+        {status === "loading" ? (
+          <div>
+            <SpinnerIcon /> Like
+          </div>
+        ) : (
+          <div>
+            {status === "liked" ? (
+              <>
+                <HeartIcon />
+                Liked
+              </>
+            ) : (
+              <>
+                <HeartIcon />
+                Like
+              </>
+            )}
+          </div>
+        )}
       </button>
+      {message}
     </div>
   );
 }
+
 /**
- * default, hover, liked, loading
- * click - request api, loading -> 50%
- * success - change to liked
- * fail - default, hovered
- * errormessage below the button
+ * red - #f00, gray - #888
+ * 1. default , clicked, loading, hover
+ * 2. success -> Liked
+ * 3. fail -> default/hovered error message below
+ * 4. api 50% chance
+ * 5. req POST body:{action :'like' or 'unlike'}
+ * 6. resp.message / statuscode 200/500
  *
+ *
+ * 1. default, loading 상태 일때 아이콘 표시
+ * 2. call api -> change state like(liked) or unlike(default)
+ * 3. implement css
  */
